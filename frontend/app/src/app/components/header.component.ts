1d251f85e6e2e0c6eb4da798ed3541da41efd9f9
Jeffry Gonzalez <jeff@hypertheory.com>
2023-07-17 15:50:30 -0400

    added tailwind

--- frontend/app/src/app/components/header.component.ts
+++ frontend/app/src/app/components/header.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     template: `
     <header>
-        <h1>Intro to Programming</h1>
+        <h1 class="text-3xl text-blue-700">Intro to Programming</h1>
         <p>Sample Full-Stack Application For The Training Class</p>
     </header>
     `
