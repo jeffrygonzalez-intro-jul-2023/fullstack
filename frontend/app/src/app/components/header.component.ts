25bca946bf6f5815a2021aff3ead6734eb9e0279
Jeffry Gonzalez <jeff@hypertheory.com>
2023-07-17 16:39:22 -0400

    done with monday

--- frontend/app/src/app/components/header.component.ts
+++ frontend/app/src/app/components/header.component.ts
@@ -4,8 +4,8 @@ import { Component } from "@angular/core";
     standalone: true,
     selector: 'app-header',
     template: `
-    <header>
-        <h1 class="text-3xl text-blue-700">Intro to Programming</h1>
+    <header class="p-4 border-b-2 border-black">
+        <h1 class="text-4xl text-primary font-black">Intro to Programming</h1>
         <p>Sample Full-Stack Application For The Training Class</p>
     </header>
     `
