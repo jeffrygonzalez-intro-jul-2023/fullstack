dde88d3dc547d24eb34856038ea1a73d69571357
Jeffry Gonzalez <jeff@hypertheory.com>
2023-07-24 14:15:30 -0400

    docker compose

---
+++ backend/TodoSolution/TodosApi/Controllers/TodoListController.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace TodosApi.Controllers;
+
+public class TodoListController : ControllerBase
+{
+    // GET /todo-list
+    [HttpGet("/todo-list")]
+    public async Task<ActionResult> GetTodoList()
+    {
+        // fake this for a moment
+        var list = new List<TodoListItemResponseModel>
+        {
+            new TodoListItemResponseModel(Guid.NewGuid(), "Get Beer", TodoItemStatus.Now),
+            new TodoListItemResponseModel(Guid.NewGuid(), "Clean Garage", TodoItemStatus.Waiting)
+        };
+
+
+        var response = new CollectionResponse<TodoListItemResponseModel>(list);
+        return Ok(response);
+    }
+}
---
+++ backend/TodoSolution/TodosApi/Models/TodoListModels.cs
@@ -0,0 +1,6 @@
+﻿namespace TodosApi.Models;
+
+public enum TodoItemStatus {  Later, Now, Waiting, Completed }
+public record TodoListItemResponseModel(Guid Id, string Description, TodoItemStatus Status);
+
+
